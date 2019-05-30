cbee7e5fe106d948fb9ca9b13ebcc20ae5f40fc6
hideyuk1-jp <hh11028+github@gmail.com>
2019-05-30 20:50:20 +0900

    ルート情報を追加して、ヒアドキュメントを表示

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+$html = <<<EOF
+<html>
+<head>
+<title>Hello</title>
+<style>
+body {font-size:16pt; color:#999;}
+h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+</style>
+<body>
+    <h1>Hello</h1>
+    <p>This is sample page.</p>
+    <p>これは、サンプルで作ったページです。</p>
+</body>
+</html>
+EOF;
+
+Route::get('hello', function () use ($html) {
+    return $html;
+});
+
