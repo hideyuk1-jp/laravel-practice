db04017c2ebe7f64cafef01d3ac83346dc703d28
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-01 07:52:44 +0900

    ルーティング

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class HelloController extends Controller
 {
-    //
+    public function index() {
+        return <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+        body { font-size:16pt; color:#999; }
+        h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
+        </style>
+        <body>
+            <h1>Index</h1>
+            <p>これは、HelloコントローラーのIndexページです。</p>
+        </body>
+        </html>
+EOF;
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,23 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{msg?}', function ($msg = "no message.") {
-
-    $html = <<<EOF
-    <html>
-    <head>
-    <title>Hello</title>
-    <style>
-    body { font-size:16pt; color:#999; }
-    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
-    </style>
-    <body>
-        <h1>Hello</h1>
-        <p>{$msg}</p>
-        <p>これは、サンプルで作ったページです。</p>
-    </body>
-    </html>
-EOF;
-
-    return $html;
-});
+Route::get('hello/', 'HelloController@index');
