4220ce98c2fd2b3c5e3d71998caf28ffa18d5520
hideyuk1-jp <hh11028+github@gmail.com>
2019-06-01 07:44:13 +0900

    HelloController作成

--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ Route::get('hello/{msg?}', function ($msg = "no message.") {
     <head>
     <title>Hello</title>
     <style>
-    body {font-size:16pt; color:#999;}
-    h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px;}
+    body { font-size:16pt; color:#999; }
+    h1 { font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px; }
     </style>
     <body>
         <h1>Hello</h1>
@@ -31,8 +31,7 @@ Route::get('hello/{msg?}', function ($msg = "no message.") {
         <p>これは、サンプルで作ったページです。</p>
     </body>
     </html>
-    EOF;
+EOF;
 
     return $html;
 });
-
